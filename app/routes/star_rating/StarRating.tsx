a54d6dae5165ed3318b3fe9d61a346db664c469b
Rushan <rushangupta99@gmail.com>
2025-11-08 21:59:20 +0530

    make star-reviews page

--- app/routes/star_rating/StarRating.tsx
+++ app/routes/star_rating/StarRating.tsx
@@ -1,7 +1,20 @@
+import { useEffect, useState } from "react";
+import { FaStar } from "react-icons/fa";
+
 export default function StarRating () {
+    const [value,setValue] = useState<number>(-1);
+    const [fixed,setFixed] = useState<number>(-1);
     return (
-        <div>
-             
+        <div className="star-rating flex flex-row justify-center items-center min-h-screen bg-gray-700">
+            {
+                [...Array(5)].map((item,index) => (
+                    <FaStar key={index} color={ index <= value ? "gold" : "#ccc" } size={120} 
+                        onClick={() =>  setFixed(index)  }
+                        onMouseEnter={() => setValue(index)}
+                        onMouseLeave={() => setValue(fixed) }
+                    /> 
+                ))
+            }
         </div>
     );
 }
