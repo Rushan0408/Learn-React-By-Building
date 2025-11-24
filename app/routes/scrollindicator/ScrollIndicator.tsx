d691901ebc5cde1a8936dbe774053ab96cd19e38
Rushan <rushangupta99@gmail.com>
2025-11-25 01:01:22 +0530

    add scroll indicator

--- app/routes/scrollindicator/ScrollIndicator.tsx
+++ app/routes/scrollindicator/ScrollIndicator.tsx
@@ -1,46 +1,68 @@
 import { useEffect, useState } from "react";
 
 interface Product {
-  id: number;
-  title: string;
+    id: number;
+    title: string;
 }
 
 interface ProductsResponse {
-  products: Product[];
-  total: number;
-  skip: number;
-  limit: number;
+    products: Product[];
+    total: number;
+    skip: number;
+    limit: number;
 }
 
 const ScrollIndicator = () => {
-  const [data, setData] = useState<ProductsResponse>();
-  const [ylen,set]
+    const [data, setData] = useState<ProductsResponse>();
+    const [progress , setProgress] = useState<number>(0);
 
-  useEffect(() => {
-    async function apicall() {
-      const res = await fetch("https://dummyjson.com/products");
-      const json = await res.json();
-      setData(json);
-    }
-    apicall();
-  }, []);
+    useEffect(() => {
+        if (typeof window === "undefined") return;
 
-  useEffect(() => console.log(data), [data]);
+        function handleScroll() {
+            const scrollTop = window.scrollY || window.pageYOffset;
+            const docHeight =
+                document.documentElement.scrollHeight - window.innerHeight;
 
-  
+            const progress = Math.abs(Math.round((scrollTop / docHeight) * 100)-1);
+            console.log(progress);
+            setProgress(progress);
+        }
 
-  return (
-    <div className="flex flex-col min-h-screen justify-center items-center">
-        <h1 className="text-blue-500 text-4xl">Scroll Indicator</h1>
-        <div>
-            { data?.products.map((item,key)=>(
-                <div key={key} className="border text-2xl p-1"> 
-                    {item.id} {item.title}
-                </div>
-            ))  }
+        window.addEventListener("scroll", handleScroll);
+
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
+
+    useEffect(() => {
+        async function apicall() {
+            const res = await fetch("https://dummyjson.com/products");
+            const json = await res.json();
+            setData(json);
+        }
+        apicall();
+    }, []);
+
+    useEffect(() => console.log(data), [data]);
+
+    return (<>
+        <div className=" flex flex-col fixed top-0 left-0 w-full bg-amber-300 p-2">
+            <div style={{ width: progress + "%" }} className="bg-blue-600 h-6 border"></div>
+            <h1 className="text-3xl text-center">Scroll Indicator</h1>
+            
         </div>
-    </div>
-  );
+        <div className="flex flex-col min-h-screen w-screen justify-center items-center mt-20">
+            <div>
+                {data?.products.map((item, key) => (
+                    <div key={key} className="border text-5xl p-1 m-3">
+                        {item.id} {item.title}
+                    </div>
+                ))}
+            </div>
+        </div>
+        </>
+    );
 };
 
 export default ScrollIndicator;
